edf1bb863148385374da167d43bf8a62e5d6d1ea
Khushal Vadher <khushal@auberginesolutions.com>
2025-01-30 13:59:14 +0530

    refactor code

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -2,19 +2,39 @@
 
 export class PasswordVerification {
   verify(password: string) {
-    if (!password || password.length == 0) {
-      throw Error("password should not be null");
-    }
-    if (password.length < 9) {
-      throw Error("password should be larger than 8 chars");
-    }
-
-    if (!/[A-Z]/.test(password)) {
-      throw Error("password should have one uppercase letter at least");
-    }
-    if (!/[a-z]/.test(password)) {
-      throw Error("password should have one lowercase letter at least");
-    }
-    throw Error("password should have one number at least");
+    notNull(password);
+    atLeastEightChars(password);
+    atLeastOneUpperCase(password);
+    atLeastOneLowerCase(password);
+    atLeastOneNumber(password);
   }
 }
+
+const notNull = (password) => {
+  if (!password || password.length == 0) {
+    throw Error("password should not be null");
+  }
+};
+
+const atLeastEightChars = (password) => {
+  if (password.length < 9) {
+    throw Error("password should be larger than 8 chars");
+  }
+};
+
+const atLeastOneUpperCase = (password) => {
+  if (!/[A-Z]/.test(password)) {
+    throw Error("password should have one uppercase letter at least");
+  }
+};
+const atLeastOneLowerCase = (password) => {
+  if (!/[a-z]/.test(password)) {
+    throw Error("password should have one lowercase letter at least");
+  }
+};
+
+const atLeastOneNumber = (password) => {
+  if (!/\d/.test(password)) {
+    throw Error("password should have one number at least");
+  }
+};
